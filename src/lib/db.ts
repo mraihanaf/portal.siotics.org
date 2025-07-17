fe85c1880836da2171896756f6e47ef9bd900761
mraihanaf <mraihanaf0@gmail.com>
2025-07-18 01:29:49 +0700

    refactor: change db connection method for more effective usage

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,23 @@
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { neon } from '@neondatabase/serverless';
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http';
+import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
 import * as AuthSchema from '@/../auth-schema';
 import * as DBSchema from "@/db/schema"
 
 const schema = { ...AuthSchema, ...DBSchema };
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-})
-const db = drizzle({ client: pool, schema: schema });
+let db: ReturnType<typeof drizzleNeon> | ReturnType<typeof drizzlePostgres>
+
+if(process.env.NODE_ENV === "test"){
+  const sql = neon(process.env.DATABASE_URL!)
+  db = drizzleNeon({ client: sql, schema: schema })
+} else {
+  db = drizzlePostgres({
+    connection: {
+      connectionString: process.env.DATABASE_URL!
+    },
+    schema: schema
+  })
+}
 
 export default db;
