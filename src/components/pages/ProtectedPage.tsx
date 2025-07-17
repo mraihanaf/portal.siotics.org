ac0c38bd6c514342c39f42e138fab4c5e6d347da
mraihanaf <mraihanaf0@gmail.com>
2025-07-18 01:32:02 +0700

    feat: url path validation

--- src/components/pages/ProtectedPage.tsx
+++ src/components/pages/ProtectedPage.tsx
@@ -4,9 +4,11 @@ import { useSession } from "@/lib/auth-client";
 import Spinner from "@/components/ui/Spinner";
 import { redirect } from "next/navigation";
 import ErrorPage from "@/components/pages/ErrorPage";
+import { usePathname } from "next/navigation";
 
 export default function ProtectedPage({ children }: { children: React.ReactNode}) {
     const { data, isPending, error } = useSession();
+    const path = usePathname()
     if(isPending) {
         return (
             <div className="flex items-center justify-center min-h-screen">
@@ -20,6 +22,8 @@ export default function ProtectedPage({ children }: { children: React.ReactNode}
     }
 
     if(!data) return redirect("/sign-in")
-    
+    if(path !== "/verify" && !data.user.emailVerified) return redirect("/verify")
+    if(path === "/verify" && data.user.emailVerified) return redirect("/")
+    if(path === "/home" && !data.user.isApplied) return redirect("/")
     if(data) return children
 }
