0619e20303f2fe1a5cccae6b6b0b58bacb5e0ee5
mraihanaf <mraihanaf0@gmail.com>
2025-07-18 01:30:26 +0700

    feat: email sending via smtp (relay)

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,5 +1,25 @@
+import nodemailer from "nodemailer"
+
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+const transporter = nodemailer.createTransport({
+	host: process.env.SMTP_HOST,
+	port: Number(process.env.SMTP_PORT),
+	secure: false, // true for 465, false for other ports
+	auth: {
+		user: process.env.SMTP_USER,
+		pass: process.env.SMTP_PASS,
+	},
+} as SMTPTransport.Options);
+
+
 export async function sendEmail({ to, subject, text }: { to: string; subject: string; text: string }) {
     console.log("Sending email", { to, subject, text });
-    // Here you would implement the actual email sending logic
-    // For example, using a service like SendGrid, Nodemailer, etc.
+    const info = await transporter.sendMail({
+        from: '"Siotics" <no-reply@siotics.org>',
+        subject: "Verify your Siotics Portal Account",
+        to,
+        text
+    })
+    console.log("Message sent!", info.messageId)
 }
